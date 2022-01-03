7322eb1c92f5603dee4ce38e596cb5849a998e09
Nassim1Omaati <2151890@talnet.nl>
2022-01-03 16:01:42 +0100

    .

---
+++ 2-Variabelen-en-datatype/2.10 - String concatenation 2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $waarde = "<li> item 1 </li>";
+    $waarde .= "<li> item 2 </li>";
+    echo $waarde .= "<li> item 3 </li>";
+    ?>
+</body>
+</html>
