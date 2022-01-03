edc6404a49486e5efffa4d324cf3b34aaebbdad9
Nassim1Omaati <2151890@talnet.nl>
2022-01-03 14:24:10 +0100

    .

--- 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -1,12 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+    
 </head>
 <body>
-    <?php
-    $string = "string"
-    echo gettype($string);
-    ?>
+<?php
+$stringo = "a string of words";
+echo gettype($stringo);
+?>
+
+<?php
+$getall = 2;
+echo gettype($getall);
+?>
+
+<?php
+$getal = 0.23;
+echo gettype($getal);
+?>
+
+<?php
+$test = true;
+echo gettype($test);
+?>
+
+<?php
+$test = null;
+echo $test;
+echo gettype($test);
+?>
+
 </body>
 </html>
