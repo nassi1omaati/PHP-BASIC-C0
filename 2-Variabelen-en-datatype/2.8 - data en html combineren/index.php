b10469e6968fd5659dc54a3baee439d7091c4149
Nassim1Omaati <2151890@talnet.nl>
2022-01-03 15:25:14 +0100

    .

---
+++ 2-Variabelen-en-datatype/2.8 - data en html combineren/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$voornaam1 = "Nassim";
+$voornaam2 = "Anass";
+$voornaam3 = "Recep";
+echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+    ?>
+</body>
+</html>
