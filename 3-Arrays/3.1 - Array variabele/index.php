5e408a06e81a2fad21dacd6927a4b11aa0ba0ed0
Nassim1Omaati <2151890@talnet.nl>
2022-01-04 14:01:59 +0100

    .

---
+++ 3-Arrays/3.1 - Array variabele/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$myMovies = ["Spiderman no way home", "endgame", "mugen train"];
+echo $myMovies [0];
+echo "<br>";
+echo $myMovies [1];
+echo "<br>";
+echo $myMovies [2];
+echo "<br>";
+
+?>
+</body>
+</html>
