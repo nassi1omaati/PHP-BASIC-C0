b6264a2d64297583409ae5fa13deee3e8a30b448
Nassim1Omaati <2151890@talnet.nl>
2022-01-09 19:18:14 +0100

    .

---
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$getal1 = 90;
+$getal1 += 7;
+echo $getal1 . "<br>";
+
+$getal1 = 88;
+$getal1 -= 12;
+echo $getal1 . "<br>";
+
+$getal1 = 10;
+$getal1 *= 5;
+echo $getal1 . "<br>";
+
+
+$getal1 = 8;
+$getal1 /= 2;
+echo $getal1 . "<br>";
+?>
+</body>
+</html>
