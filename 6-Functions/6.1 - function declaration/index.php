bad684f02debefe2c0482fef58f2d8477f4c2a5c
Nassim1Omaati <2151890@talnet.nl>
2022-01-10 11:02:27 +0100

    .

---
+++ 6-Functions/6.1 - function declaration/index.php
@@ -0,0 +1,12 @@
+<?php
+    
+function zetTweeWoordenAanElkaar(){
+    $woordje = "Hallo";
+    $zinnetje = "mooie blauwe planeet!";
+    $helezin = $woordje . " " . $zinnetje;
+    echo ($helezin);
+
+} 
+
+zetTweeWoordenAanElkaar();
+?>
