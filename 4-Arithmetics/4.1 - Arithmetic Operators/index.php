c8c4c3b21e9987fb5b5f76dfef2b19f11388c2f0
Nassim1Omaati <2151890@talnet.nl>
2022-01-26 21:55:51 +0100

    .

--- 4-Arithmetics/4.1 - Arithmetic Operators/index.php
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   <?php
-   $getal1= 1;
-   $getal2 = 419;
-   echo $getal1 + $getal2;
-   
-   ?> 
-</body>
-</html>
+<?php
+$getal1= 10;
+$getal2 = 5;
+echo $getal1 + $getal2 . " ";
+echo $getal1 - $getal2 . " ";
+echo $getal1 * $getal2 . " ";
+echo $getal1 / $getal2;
+?>
